2c574a564d1f37720d9182ad23b92ed1579c8160
toshiakisan1127 <toshiakisan1127@gmail.com>
2021-05-02 20:02:31 +0900

    カスタムdirectiveの練習。

--- 5/5-3/app.js
+++ 5/5-3/app.js
@@ -1,11 +1,32 @@
 // const { default: Vue } = require("vue");
 
 Vue.directive("fallback-image", {
-  bind: function (el) {
-    el.addEventListener("error", function () {
-      el.src = "http://dummyimage.com/400x400/000/ffffff.png&text=no+image";
+  bind: function (el, binding) {
+    console.log("bind", binding);
+    let once = binding.modifiers.once;
+    el.addEventListener("error", function onError() {
+      el.src = binding.value;
+      if (once) {
+        el.removeEventListener("error", onError);
+      }
+      //   el.src = "http://dummyimage.com/400x400/000/ffffff.png&text=no+image";
     });
+  },
+  update: function (el, binding) {
+    console.log("update", binding);
+    if (binding.oldValue !== binding.value && binding.oldValue === el.src) {
+      el.src = binding.value;
+    }
+    // console.log("hoge");
   },
 });
 
-new Vue({ el: "#app" });
+let vm = new Vue({
+  el: "#app",
+  data() {
+    return {
+      altText: "logo",
+      noImageURL: "http://dummyimage.com/400x400/000/ffffff.png&text=no+image",
+    };
+  },
+});
